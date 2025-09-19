25e32fc116852b3d0ed4f53dd4869abbe75a856e
blackwalt608 <merteliacik@hotmail.com>
2025-09-19 22:12:33 +0300

    last edit

--- src/app/auth/new-password/page.tsx
+++ src/app/auth/new-password/page.tsx
@@ -1,9 +1,14 @@
 "use client";
 
 import { NewPasswordForm } from "@/components/auth/new-password-form";
+import { Suspense } from "react";
 
 export const dynamic = "force-dynamic";
 
 export default function NewPass() {
-  return <NewPasswordForm />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <NewPasswordForm />
+    </Suspense>
+  );
 }
