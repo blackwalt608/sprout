df2dbe33cf3029aa462bba5fa095cd0f0593d2e3
blackwalt608 <merteliacik@hotmail.com>
2025-09-19 21:39:56 +0300

    edit update

--- src/utils/base-url.ts
+++ src/utils/base-url.ts
@@ -1,5 +1,5 @@
 export default function getBaseURL() {
   if (typeof window !== "undefined") return "";
-  if (process.env.VERCEL_URL) return `https://${process.env.DOMAIN_URL}`;
-  ("http://localhost:3000");
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  return "http://localhost:3000";
 }
