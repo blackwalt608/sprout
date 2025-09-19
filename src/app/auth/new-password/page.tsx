489d83e7a640349b6ebe97cb365f2659eae0ca88
blackwalt608 <merteliacik@hotmail.com>
2025-09-19 22:08:25 +0300

    asd

--- src/app/auth/new-password/page.tsx
+++ src/app/auth/new-password/page.tsx
@@ -1,5 +1,8 @@
 "use client";
+
 import { NewPasswordForm } from "@/components/auth/new-password-form";
+
+export const dynamic = "force-dynamic";
 
 export default function NewPass() {
   return <NewPasswordForm />;
